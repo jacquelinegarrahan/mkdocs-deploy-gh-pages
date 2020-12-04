c76f7c6de07823a5c3eb796c091799170d428342
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-12-03 17:07:57 -0800

    Set working directory

--- action.sh
+++ action.sh
@@ -6,6 +6,8 @@ function print_info() {
     echo -e "\e[36mINFO: ${1}\e[m"
 }
 
+cd $HOME
+
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p "$HOME"/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
